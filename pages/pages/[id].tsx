5dadfc9567d0b2e865f072d8059ac31fbaa943ec
Denis Barvitskiy <bedenz@gmail.com>
2021-02-18 10:07:15 +0700

    #0000 fix: api fix

--- pages/pages/[id].tsx
+++ pages/pages/[id].tsx
@@ -47,7 +47,7 @@ export async function getStaticProps({ params }) {
   //  const { menu, meta, title, ver } = result
 
   return {
-    props: { ...result, url },
+    props: { ...result, url, params },
   }
 }
 
